c4e07c520fdb2033a160fece5d5f5671ca811e03
onetwotimer <jjose21@stac.edu>
2025-10-03 15:46:52 -0400

    Fix config errors for Image

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "ktixltgtvmdrtvsvjyrq.supabase.co", // your Supabase project hostname
+      },
+    ],
+  },
 };
 
 export default nextConfig;
